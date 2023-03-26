ec952f625cc6ac012c9ba1cf6a8a94e19f693bda
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-27 00:39:24 +0300

    create App file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const App = () => {
-  return (
-    <div>
-      <textarea autoFocus={true} spellCheck={true} /> <br />
-      <input type="number" min={5} style={{ color: "blue" }} />
-    </div>
-  );
-};
+
 root.render(
   <React.StrictMode>
     <App />
