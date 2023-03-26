898871c775c13f774dbf46f71de2f564c1a02ccb
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-27 00:04:53 +0300

    adding message test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const App = () => {
-  return <h1>Hi there!</h1>;
+  let message = "Bye there!";
+  if (Math.random() > 0.5) {
+    message = "Hello there!";
+  }
+  return <h1>{message}</h1>;
 };
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
