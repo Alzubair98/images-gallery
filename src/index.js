dcf5eac6d55897068c577979c683fbd272452231
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-26 23:54:27 +0300

    write index code

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const App = () => {
+  return <h1>Hi there!</h1>;
+};
+root.render(<App />);
