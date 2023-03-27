505d06d4b3149f99e93f338cbef1c6898d33194d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-28 02:55:24 +0300

    add alt props

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const ProfileCard = ({ title, handle }) => {
+const ProfileCard = ({ title, handle, image }) => {
   //   const { title, handle } = props;
 
   return (
     <>
+      <img src={image} alt="images" />
       <div>Title is {title} </div>
       <div>Hadle is {handle}</div>
     </>
