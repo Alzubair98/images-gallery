25604f086364d843ce2292e10c88df8c4c6f9077
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-28 02:33:43 +0300

    add images

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./alexa.png";
+import CortanaImage from "./cortana.png";
+import Siri from "./siri.png";
 
 const App = () => {
   return (
     <div>
       <div>Persoal Digita Assistats</div>
-      <ProfileCard title="Alexa" handle="@Alexa" />
-      <ProfileCard title="Cortana" handle="@Cortana" />
-      <ProfileCard title="Siri" handle="@Siri" />
+      <ProfileCard title="Alexa" handle="@Alexa" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@Cortana" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@Siri" image={Siri} />
     </div>
   );
 };
