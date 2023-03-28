3008058bb20679423fa95a9afd848eb4863d03f8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-28 13:25:51 +0300

    style cards

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,14 +1,23 @@
 import React from "react";
-
+import "bulma/css/bulma.css";
 const ProfileCard = ({ title, handle, image }) => {
   //   const { title, handle } = props;
 
   return (
-    <>
-      <img src={image} alt="images" />
-      <div>Title is {title} </div>
-      <div>Handle is {handle}</div>
-    </>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-cotent">
+          <p className="title is-4">Title is {title} </p>
+          <p className="subtitle is-6">Handle is {handle}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./alexa.png";
 import CortanaImage from "./cortana.png";
@@ -8,9 +9,25 @@ const App = () => {
   return (
     <div>
       <div>Persoal Digita Assistats</div>
-      <ProfileCard title="Alexa" handle="@Alexa" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@Cortana" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@Siri" image={Siri} />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@Alexa" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@Cortana"
+                image={CortanaImage}
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@Siri" image={Siri} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
