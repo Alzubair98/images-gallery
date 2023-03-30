fbdae34c4b6c6ee0276f54063655dd3ccfc60a1f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-30 23:25:31 +0300

    ADD some margin

--- src/app.js
+++ src/app.js
@@ -7,8 +7,11 @@ const App = () => {
   };
   return (
     <div>
-      <button className="button"> Add Animal</button>
-      <p>Animal to show</p>
+      <button onClick={handleClick} className="button mt-4 ml-2">
+        {" "}
+        Add Animal
+      </button>
+      <p className="ml-2">Animal to show</p>
     </div>
   );
 };
