f4bc6f4f78c4835bc44154da7e671e31a5d2265b
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-31 20:39:49 +0300

    import images

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,4 +1,11 @@
 import React from "react";
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
 
 const AnimalShow = ({ type }) => {
   return (
