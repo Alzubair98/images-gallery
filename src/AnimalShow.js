ed42d814abf8e971d8474a8b07a8c5e7ea7c9006
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-03-31 21:05:57 +0300

    add the heart

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import bird from "./svg/bird.svg";
 import cat from "./svg/cat.svg";
 import cow from "./svg/cow.svg";
@@ -17,11 +17,15 @@ const svgMap = {
 };
 
 const AnimalShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
   return (
     <>
-      <div>
-        {" "}
+      <div onClick={handleClick}>
         <img alt="animal" src={svgMap[type]} />
+        <img alt="heart" src={heart} style={{ width: 10 + 10 * clicks }} />
       </div>
     </>
   );
