09412c6073ef58d2c1e7a197f08df318c2baced3
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-01 16:57:59 +0300

    check the function of search

--- src/api.js
+++ src/api.js
@@ -4,14 +4,18 @@ import axios from "axios";
 const link = "https://api.unsplash.com/search/photos";
 const Access_key = "ZA4eFftvCFPV-9rKFgqp16oHztvBzqz7iW1W-iuq0DI";
 const searchImages = async () => {
-  const response = await axios.get(link, {
-    headers: {
-      Authorization: `Client-ID ${Access_key}`,
-    },
-    params: {
-      query: "cars",
-    },
-  });
+  const response = await axios
+    .get(link, {
+      headers: {
+        Authorization: `Client-ID ${Access_key}`,
+      },
+      params: {
+        query: "cars",
+      },
+    })
+    .catch((error) => console.log("api error", error));
+
+  console.log(response);
 
   return response.data;
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
+import searchImages from "./api";
 
+searchImages();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
