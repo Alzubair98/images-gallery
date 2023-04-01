2dc160da8ff30ce8c304beb582c4975efb0f6396
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-01 16:04:45 +0300

    remove files

--- src/app.js
+++ src/app.js
@@ -1,28 +1,12 @@
 import React, { useState } from "react";
 import "bulma/css/bulma.css";
-import AnimalShow from "./AnimalShow";
+
 import "./app.css";
 
-const getRandomAnimal = () => {
-  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
-
-  return animals[Math.floor(Math.random() * animals.length)];
-};
-
 const App = () => {
-  const [animals, setAnimals] = useState([]);
-
-  const handleClick = () => {
-    setAnimals([...animals, getRandomAnimal()]);
-  };
   return (
     <div className="pl-2 pt-2 app">
-      <button onClick={handleClick}> Add Animal</button>
-      <div className="animal-list">
-        {animals.map((animal, index) => {
-          return <AnimalShow type={animal} key={index} />;
-        })}
-      </div>
+      <h1>hello</h1>
     </div>
   );
 };
