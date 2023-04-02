13a2525f6452ce960b82f02c6bc90019acda29ed
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-02 10:13:07 +0300

    using function to send props ups and downs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import "bulma/css/bulma.css";
 import SearchBar from "./components/Searchbar";
+import ImageList from "./components/ImageList";
 
 import "./app.css";
 
 const App = () => {
+  const handleSubmit = (term) => {
+    console.log("do a search with", term);
+  };
   return (
     <div className="pl-2 pt-2 app">
       <h1>hello</h1>
-      <SearchBar />
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageList />
     </div>
   );
 };
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import "bulma/css/bulma.css";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = (props) => {
+  const handleClick = () => {
+    props.onSubmit("boys");
+  };
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick} className="button">
+        {" "}
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
