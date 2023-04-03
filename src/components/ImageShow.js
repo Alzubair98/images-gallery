5436869abc197218510467fb3f3cd592e8bba6a8
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-03 09:28:12 +0300

    add button and card

--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "bulma/css/bulma.css";
+import "./ImageShow.css";
 
 const ImageShow = ({ image }) => {
   return (
-    <div>
+    <div className="image-card">
       <img src={image.urls.small} alt={image.alt_description} />
+      <button>Click me</button>
     </div>
   );
 };
